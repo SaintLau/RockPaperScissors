56f3de19b219b550b4162f9a8321279d8ce0c525
Laura Santos <alsr16p@gmail.com>
2021-09-25 17:19:02 +0100

    added lizard and spock

--- index.js
+++ index.js
@@ -9,29 +9,45 @@ function compare(choice1, choice2) {
     if (choice1 === choice2) {
         return "It\s a tie!";
 
-    } else if (choice1 === "rock") { //if Rock - beats scissor, defeat by paper
-        if (choice2 === "scissors") {
+    } else if (choice1 === "rock") { //if Rock - beats scissor and lizard, defeat by paper and spock
+         if (choice2 === "scissors" || choice2 === "lizard") {
             userScore++;
             return "User";
-        } else if (choice2 === "paper") {
+        } else if (choice2 === "paper" || choice2 === "spock") {
             computerScore++;
             return "Computer";
         }
-    } else if (choice1 === "paper") { //if Paper - lose to scissor, win to rock
-        if(choice2 === "scissors") {
+    } else if (choice1 === "paper") { //if Paper - lose to scissor and lizard, win to rock and spock
+        if(choice2 === "scissors" || choice2 === "lizard") {
             computerScore++;
             return "Computer";
-        } else if (choice2 === "rock") {
+        } else if (choice2 === "rock" || choice2 === "spock") {
             userScore++;
             return "User";
         }
-    } else if (choice1 === "scissors") { //if Scissor - cut paper (win), lose to rock
-        if (choice2 === "paper") {
+    } else if (choice1 === "scissors") { //if Scissor - cut paper (win) and lizard (win), lose to rock and spock
+        if (choice2 === "paper" || choice2 === "lizard") {
             userScore++;
             return "User";
-        } else if (choice2 === "rock") {
+        } else if (choice2 === "rock" || choice2 === "spock") {
             computerScore++;
             return "Computer"
+        }
+    } else if (choice1 === "spock") {
+        if(choice2 === "scissors" || choice2 === "rock") {
+            userScore++;
+            return "User";
+        } else if (choice2 === "paper" || choice2 === "lizard") {
+            computerScore++;
+            return "Computer";
+        }
+    } else if (choice1 === "lizard") {
+        if (choice2 === "spock" || choice2 === "paper") {
+            userScore++;
+            return "User";
+        } else if (choice2 === "scissors" || choice2 === "rock") {
+            computerScore++;
+            return "Computer";
         }
     }
 }
@@ -39,8 +55,8 @@ function compare(choice1, choice2) {
 //to start game:
 function startGame(e) {
     let userChoice = e;
-    let computerChoices = ["rock", "paper", "scissors"]; //pc will make its choice
-    let computerChoice = computerChoices[Math.floor(Math.random() * 3)];
+    let computerChoices = ["rock", "paper", "scissors", "lizard", "spock"]; //pc will make its choice
+    let computerChoice = computerChoices[Math.floor(Math.random() * 5)];
 
     let winner = compare(userChoice, computerChoice);
     whoWins.classList = "";
